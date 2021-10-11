2cec58b7bee8ba579bcb81decfc23df7fb9a0a42
Luc Patiny <luc@patiny.com>
2021-10-11 07:42:38 +0200

    docs: fix function jsdoc argument type

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const toString = Object.prototype.toString;
 
 /**
  * Checks if an object is an instance of an Array (array or typed array)
- * @param {object} object to check
+ * @param {any} object to check
  * @returns {boolean}
  */
 export default function isAnyArray(object) {
