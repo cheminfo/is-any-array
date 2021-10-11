035449fe2b7e2c6a9539347b5fe800458ef5bdc0
Luc Patiny <luc@patiny.com>
2021-10-11 07:29:05 +0200

    docs: add jsdoc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const toString = Object.prototype.toString;
 
+/**
+ * Checks if an object is an instance of an Array (array or typed array)
+ * @param {object} object to check
+ * @returns {boolean}
+ */
 export default function isAnyArray(object) {
   return toString.call(object).endsWith('Array]');
 }
