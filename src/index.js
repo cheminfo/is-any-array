8eca8b412da0b44e5c150afe44c35870bc9c708b
Michaël Zasso <targos@protonmail.com>
2021-10-15 20:24:40 +0200

    chore: update eslint config

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const toString = Object.prototype.toString;
 /**
  * Checks if an object is an instance of an Array (array or typed array).
  *
- * @param value - Object to check.
- * @returns True if the object is an array.
+ * @param {any} value - Object to check.
+ * @returns {boolean} True if the object is an array.
  */
 export function isAnyArray(value) {
   return toString.call(value).endsWith('Array]');
